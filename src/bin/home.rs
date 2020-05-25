39ae8f15c1d49919e9cfbbce0f979a7e9ab573b8
extrasharp <genericpb@gmail.com>
2020-05-25 09:28:12 -0500

    more home

--- src/bin/home.rs
+++ src/bin/home.rs
@@ -3,48 +3,74 @@ use std::io;
 use std::io::{Write};
 use std::path::{Path, PathBuf, Component};
 
-fn print_path(p: &str) {
-    print!("{}/", p);
-    io::stdout().flush().unwrap();
+enum TruncateError {
+    EnvHomeNotFound,
 }
 
-fn main() {
-    let trunc = 2;
-
-    let pwd = env::current_dir().unwrap();
-    if pwd == Path::new("/") {
-        print_path("");
-        return;
-    }
-
+fn truncate_path(pb: PathBuf, trunc: usize) -> Result<PathBuf, TruncateError> {
     let home =
         if let Ok(home_str) = env::var("HOME") {
             PathBuf::from(home_str)
         } else {
-            print!("env var: $HOME not found - ");
-            return;
+            return Err(TruncateError::EnvHomeNotFound);
         };
 
     let path =
-        if pwd == home {
-            print_path("~");
-            return;
-        } else if pwd.starts_with(&home) {
-            Path::new("~").join(pwd.strip_prefix(home).unwrap())
+        if pb == home {
+            return Ok(PathBuf::from("~"));
+        } else if pb.starts_with(&home) {
+            Path::new("~").join(pb.strip_prefix(home).unwrap())
         } else {
-            pwd
+            pb
         };
 
     let path_len = path.iter().count();
     let mut components = path.components();
 
-    let trunced: PathBuf =
-        if (path_len <= trunc + 1 && components.next().unwrap() == Component::RootDir)
-           || path_len <= trunc {
-            path
-        } else {
-            path.iter().skip(path_len - trunc).collect()
+    if (path_len <= trunc + 1 && components.next().unwrap() == Component::RootDir)
+       || path_len <= trunc {
+        Ok(path)
+    } else {
+        Ok(path.iter().skip(path_len - trunc).collect())
+    }
+}
+
+fn print_path(p: &str) {
+    print!("{}/", p);
+    io::stdout().flush().unwrap();
+}
+
+fn usage() {
+    println!("usage: home [show (default 2)]");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let trunc =
+        match args.get(1) {
+            Some(arg) => {
+                if let Ok(val) = arg.parse::<usize>() {
+                    val
+                } else {
+                    println!("error: cannot parse \"{}\" as a number", arg);
+                    usage();
+                    print_path("---");
+                    return;
+                }
+            },
+            None => 2
         };
 
-    print_path(trunced.to_str().unwrap());
+    let pwd = env::current_dir().unwrap();
+
+    match truncate_path(pwd, trunc) {
+        Ok(pb) => print_path(pb.to_str().unwrap()),
+        Err(err) => match err {
+            TruncateError::EnvHomeNotFound => {
+                println!("env var: $HOME not found - ");
+                usage();
+                print_path("---");
+            }
+        }
+    }
 }
